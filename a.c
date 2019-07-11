80e98702e0a9de92f3bff73644aa5aa94033e08b
宮腰力矢 <miyakoshirikiya@miyakoshichikarayanoMacBook-puro.local>
2019-07-11 22:25:37 +0900

    added the greeting

--- a.c
+++ a.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 int main(){
 int i,j,k;
+char name[20];
+srand(time(NULL));
+printf("What is your name? \n");
+scanf("%s",name);
+printf("Hello, %s!\n",name);
 printf("Rolling the dice...\n");
 i=rand()%6+1;
 printf("Die 1: %d\n",i);
